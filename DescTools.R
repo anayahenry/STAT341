73042307bb4b5a3f5a0c6b8207f03965312cd6ef
anayahenry <143841494+anayahenry@users.noreply.github.com>
2023-10-09 15:36:56 -0400

    Add files via upload

---
+++ DescTools.R
@@ -0,0 +1,25 @@
+> variablel <- c(1,23,4,5,6,7,8,9,10,1900)
+> mean(variablel)
+[1] 197.3
+
+> median(variablel)
+[1] 7.5
+
+> sd(variablel)
+[1] 598.2965
+
+> install.packages (('DescTools'))
+
+The downloaded binary packages are in
+	/var/folders/mh/9h7hkhbx6n5cr2plh185r23h0000gn/T//RtmpqGlUX8/downloaded_packages
+
+> Mode(variablel)
+[1] 1
+
+> min(variablel)
+[1] 1
+> max(variablel)
+[1] 1900
+> var(variablel)
+[1] 357958.7
+
